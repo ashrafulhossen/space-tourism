4d14f5e7df256e060fa622ed1894fe44778a1145
ashrafulhossen <b.asrafulhossencht@gmail.com>
2023-08-08 00:20:42 +0600

    navbar is done with responsiveness

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,10 +1,98 @@
 // eslint-disable-next-line no-unused-vars
-import React from 'react'
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+import menuCloseImg from "/assets/shared/icon-close.svg";
+import menuOpenImg from "/assets/shared/icon-hamburger.svg";
+import logo from "/assets/shared/logo.svg";
 
 const NavBar = () => {
-  return (
-    <div>NavBar</div>
-  )
-}
+	const [showMenu, setShowMenu] = useState(false);
 
-export default NavBar
+	const showOrCloseMenubar = () => {
+		setShowMenu(!showMenu);
+	};
+
+	const menuItems = (
+		<>
+			<li>
+				<NavLink
+					to={"/"}
+					className={({ isActive }) =>
+						`navlink ${isActive && "active-navlink"}`
+					}
+				>
+					<span>00</span>
+					Home
+				</NavLink>
+			</li>
+			<li>
+				<NavLink
+					to={"/destination"}
+					className={({ isActive }) =>
+						`navlink ${isActive && "active-navlink"}`
+					}
+				>
+					<span>01</span>
+					Destination
+				</NavLink>
+			</li>
+			<li>
+				<NavLink
+					to={"/crew"}
+					className={({ isActive }) =>
+						`navlink ${isActive && "active-navlink"}`
+					}
+				>
+					<span>02</span>
+					Crew
+				</NavLink>
+			</li>
+			<li>
+				<NavLink
+					to={"/technology"}
+					className={({ isActive }) =>
+						`navlink ${isActive && "active-navlink"}`
+					}
+				>
+					<span>03</span>
+					Technology
+				</NavLink>
+			</li>
+		</>
+	);
+
+	return (
+		<div className="navbar">
+			<div className="logo-container">
+				<img className="logo" src={logo} alt="logo image" />
+			</div>
+			<ul className="menubar">{menuItems}</ul>
+			<div className="mobile-menubar-container">
+				<button
+					className={`show-mobile-menubar ${showMenu && "hide"}`}
+					onClick={showOrCloseMenubar}
+				>
+					<img src={menuOpenImg} alt="menu open image" />
+				</button>
+
+				<div
+					className={`mobile-menubar ${
+						showMenu && "active-mobile-menubar"
+					}`}
+				>
+					<button
+						className={`close-mobile-menubar ${
+							showMenu && "active"
+						}`}
+						onClick={showOrCloseMenubar}
+					>
+						<img src={menuCloseImg} alt="menu close image" />
+					</button>
+					<ul className="mobile-menu">{menuItems}</ul>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default NavBar;
--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Main from "./Layouts/Main.jsx";
+import Crew from "./Page/Crew.jsx";
+import Destination from "./Page/Destination.jsx";
+import Home from "./Page/Home.jsx";
+import Technology from "./Page/Technology.jsx";
 import "./index.scss";
 
 const router = createBrowserRouter([
-	{ path: "/", element: <Main />, children: [] }
+	{
+		path: "/",
+		element: <Main />,
+		children: [
+			{ path: "/", element: <Home /> },
+			{ path: "/destination", element: <Destination /> },
+			{ path: "/crew", element: <Crew /> },
+			{ path: "/technology", element: <Technology /> }
+		]
+	}
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
